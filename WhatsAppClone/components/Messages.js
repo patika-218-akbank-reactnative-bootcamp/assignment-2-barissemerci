d8690d7e60f499278c23ea5c3a10818e4509c5ae
barissemerci <barissemerci3@gmail.com>
2022-08-23 23:11:47 +0300

    added Messages.js

---
+++ WhatsAppClone/components/Messages.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-localSS
+ */
+
+ import React from 'react';
+ import {StyleSheet, View,Text, ScrollView} from 'react-native';
+ import IconSearch from 'react-native-vector-icons/Fontisto'
+ import IconThreeDots from 'react-native-vector-icons/Entypo'
+
+ const Messages = () => (
+    <ScrollView style={styles.container}>
+
+
+    </ScrollView>
+ );
+ 
+ const styles = StyleSheet.create({
+  container:{
+ 
+  }
+ });
+ 
+ export default Messages;
+ 
