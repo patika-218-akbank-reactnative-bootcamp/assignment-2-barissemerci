b8f38104c5731e46ae52a735098b64b6505c22b2
barissemerci <barissemerci3@gmail.com>
2022-08-23 23:11:20 +0300

    updated Menu

--- WhatsAppClone/components/Menu.js
+++ WhatsAppClone/components/Menu.js
@@ -1,45 +1,63 @@
 
-import React from 'react';
 import {Text,View,Image,StyleSheet,TouchableOpacity,ScrollView} from 'react-native';
 import IconCamera from 'react-native-vector-icons/MaterialIcons'
+import React, { useState, useEffect } from 'react';
+
 
 const Menu = () =>{
+    const [selectedTab, setselectedTab] = useState('SOHBETLER');
+    const [tabs, setTabs] = useState(['KAMERA','SOHBETLER','DURUM','ARAMALAR']);
+
     return (
         <View style={styles.container}>
 
-         
-        <TouchableOpacity>
-            <View style={{width:40,height:40,alignItems:'center',justifyContent:'center'}}>
-            <IconCamera style={styles.icon} size={25} name="photo-camera"></IconCamera>
-            </View>
            
-        </TouchableOpacity>
-        <View style={styles.menuContainer}>
 
-        <TouchableOpacity style={styles.touchableOpacity}>
-        <View style={styles.menuItemsStyle}>
-            <Text style={styles.text}>
-                SOHBETLER
-            </Text>
-          </View>  
-        </TouchableOpacity> 
+{
+    tabs.map((item,index)=> {
+
+        if (item==='KAMERA') {
+            return  <TouchableOpacity onPress={() => {
+                setselectedTab(item);
+              }}>
+            <View style={styles.cameraMenuStyle}>
+            <IconCamera style={item == selectedTab ? styles.iconSelected: styles.icon} size={25} name="photo-camera"></IconCamera>
+            </View>
+            <View style={item == selectedTab ? styles.cameraSelectedLine : {}}></View>
+
+        </TouchableOpacity>;
+          }
+          else{
+            return (
+                    <TouchableOpacity  onPress={() => {
+                setselectedTab(item);
+              }} style={styles.touchableOpacity}>
+            <View style={styles.menuItemsStyle}>
+                <Text style={item == selectedTab ? styles.textSelected : styles.text}>
+                    {item}
+                </Text>
+              </View>  
+              <View style={item == selectedTab ? styles.line : {}}></View>
+            </TouchableOpacity>
+            )
+          }
+
+
+
+
         
-        <TouchableOpacity style={styles.touchableOpacity}>
-        <View style={styles.menuItemsStyle}>
-        <Text style={styles.text}>
-                DURUM
-            </Text>
-            </View>
-        </TouchableOpacity> 
+    }
+    
+    )
+}
+         
+      
        
-        <TouchableOpacity style={styles.touchableOpacity}> 
-        <View style={styles.menuItemsStyle}>
-        <Text style={styles.text}>
-                ARAMALAR
-            </Text>
-            </View>
-        </TouchableOpacity>
-        </View>
+
+        
+        
+
+       
           </View>
          
         
@@ -49,10 +67,13 @@ const Menu = () =>{
 
 const styles = StyleSheet.create({
    container:{
-    flexDirection:'row',    
+    flexDirection:'row',   
+    paddingLeft:5, 
     backgroundColor:'#008069',
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    paddingTop:10,
+    paddingBottom:5
 
    },
    text:{
@@ -65,15 +86,35 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     
    },
-   menuContainer:{
-    flexDirection:'row', 
-    flex:3,
-   },
+   
    touchableOpacity:{
     flex:1,
+
    },
    icon:{
     color:'#aad5ce',
+   },
+   line:{
+    alignSelf:'center',
+    height:2,
+   width:100,
+    backgroundColor:'white',
+    
+   },
+   textSelected:{
+    color:'white',
+    fontWeight:'800',
+    fontSize:16,
+   },
+   iconSelected:{
+    color:'white',
+   },
+   cameraSelectedLine:{
+    height:2,backgroundColor:'white'
+   },
+   cameraMenuStyle:{
+    justifyContent:'center',
+    alignItems:'center'
    }
 
   });
