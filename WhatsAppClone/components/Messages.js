a17f538390aa9628f2dc492728a7d75faa91f272
barissemerci <barissemerci3@gmail.com>
2022-08-25 00:15:05 +0300

    message.js updated

--- WhatsAppClone/components/Messages.js
+++ WhatsAppClone/components/Messages.js
@@ -7,16 +7,47 @@
  */
 
  import React from 'react';
- import {StyleSheet, View,Text, ScrollView} from 'react-native';
- import IconSearch from 'react-native-vector-icons/Fontisto'
- import IconThreeDots from 'react-native-vector-icons/Entypo'
-
- const Messages = () => (
-    <ScrollView style={styles.container}>
+ import {StyleSheet,Button, View,Text, ScrollView, FlatList} from 'react-native';
+ import { useNavigation } from '@react-navigation/native';
+ import chat_data from 'WhatsAppClone/data/chat-data.json'
+ import MessageCard from './MessageCard';
 
 
-    </ScrollView>
- );
+
+
+ const Messages = (props) => {
+
+   const navigation = useNavigation(); 
+
+
+   const renderMessageCard=({item}) =><MessageCard message={item} />
+
+
+
+   function navigateChatPage(){
+      navigation.navigate('ChatPage')
+    }
+
+
+
+   return(
+      <FlatList
+      data={props.messages}
+      renderItem={renderMessageCard}
+      keyExtractor={item => item.id}
+    />
+      
+         );
+
+ };
+
+   
+
+
+ 
+
+    
+
  
  const styles = StyleSheet.create({
   container:{
