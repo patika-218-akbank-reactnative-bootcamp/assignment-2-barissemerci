1ddfd4001553bdcc94df7fec5d91c2f00b810c23
barissemerci <barissemerci3@gmail.com>
2022-08-24 21:11:18 +0300

    init ChatPage.js

---
+++ WhatsAppClone/components/ChatPage.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-localSS
+ */
+
+ import React from 'react';
+ import {StyleSheet, View,Text, ScrollView} from 'react-native';
+ import IconSearch from 'react-native-vector-icons/Fontisto'
+ import IconThreeDots from 'react-native-vector-icons/Entypo'
+
+ const ChatPage = () => (
+    <ScrollView style={styles.container}>
+<Text>CHAT PAGE</Text>
+
+    </ScrollView>
+ );
+ 
+ const styles = StyleSheet.create({
+  container:{
+ 
+  }
+ });
+ 
+ export default ChatPage;
+ 
